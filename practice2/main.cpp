a81e5dd0e5c5d717c92786cebc3f2c1825ba81e2
Arne Bahlo <hallo@arne.me>
2014-04-07 11:28:51 +0200

    Add emblem

--- practice2/main.cpp
+++ practice2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include <exception>
 
 #include "colors.h"
@@ -8,7 +9,15 @@ using namespace std;
 int main() {
   try {
     string input = "";
+
+    cout << BLUE << " _______            _" << endl;
+    cout <<           "|__   __|          | |" << endl;
+    cout <<           "   | |_ __ __ _ ___| |__" << endl;
+    cout <<           "   | | '__/ _` / __| '_ \\" << endl;
+    cout <<           "   | | | | (_| \\__ \\ | | |" << endl;
+    cout <<           "   |_|_|  \\__,_|___/_| |_|" << RESET << endl << endl;
     cout << BLUE << "Welcome to Trash! Exit with <logout>" << RESET << endl;
+
     while (true) {
       cout << "$ ";
       cin >> input;
@@ -16,7 +25,9 @@ int main() {
       if (input == "logout") {
         break;
       } else if (input != "") {
-        cout << "COMMAND: " << input << endl;
+        const char *cInput = input.c_str();
+        cout << YELLOW << cInput << endl;
+        execlp(cInput, cInput, NULL);
       }
     }
   } catch(const string &e) {
