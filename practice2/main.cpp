8fe7cbb8c6883d4c2a1ab185adc47eabe405ec97
Arne Bahlo <hallo@arne.me>
2014-04-07 12:07:36 +0200

    Parse arguments in vector, transform to char array

--- practice2/main.cpp
+++ practice2/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <unistd.h>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 #include <exception>
 
 #include "colors.h"
 
 using namespace std;
 
+// Helper function to convert from string to char
+char *convert(const std::string & s) {
+   char *pc = new char[s.size()+1];
+   std::strcpy(pc, s.c_str());
+   return pc;
+}
+
 int main() {
   try {
     string input = "";
 
+    // Print emblem
     cout << BLUE << " _______            _" << endl;
-    cout <<           "|__   __|          | |" << endl;
-    cout <<           "   | |_ __ __ _ ___| |__" << endl;
-    cout <<           "   | | '__/ _` / __| '_ \\" << endl;
-    cout <<           "   | | | | (_| \\__ \\ | | |" << endl;
-    cout <<           "   |_|_|  \\__,_|___/_| |_|" << RESET << endl << endl;
+    cout <<         "|__   __|          | |" << endl;
+    cout <<         "   | |_ __ __ _ ___| |__" << endl;
+    cout <<         "   | | '__/ _` / __| '_ \\" << endl;
+    cout <<         "   | | | | (_| \\__ \\ | | |" << endl;
+    cout <<         "   |_|_|  \\__,_|___/_| |_|" << RESET << endl << endl;
+    // Print welcome message
     cout << BLUE << "Welcome to Trash! Exit with <logout>" << RESET << endl;
 
     while (true) {
@@ -25,9 +38,32 @@ int main() {
       if (input == "logout") {
         break;
       } else if (input != "") {
-        const char *cInput = input.c_str();
-        cout << YELLOW << cInput << endl;
-        execlp(cInput, cInput, NULL);
+        // Split string
+        input = "ls -la /home/arne";
+        istringstream iss(input);
+
+        // Split into arguments
+        vector<string> arguments;
+        copy(istream_iterator<string>(iss),
+                 istream_iterator<string>(),
+                 back_inserter<vector<string> >(arguments));
+
+        // Rotate vector to the left, to get the command at the end,
+        // so we can pop_back it
+        string command = arguments.at(0);
+        std::rotate(arguments.begin(), arguments.begin() + 1, arguments.end());
+        arguments.pop_back();
+
+        // Transform arguments vector<string> to char *const *
+        char ** crguments = new char*[arguments.size()];
+        for(size_t i = 0; i < arguments.size(); ++i){
+            crguments[i] = new char[arguments[i].size() + 1];
+            strcpy(crguments[i], arguments[i].c_str());
+        }
+
+        cout << YELLOW << arguments.size() << RESET << endl;
+        cout << GREEN << command << RESET << endl;
+        execvp(command.c_str(), crguments);
       }
     }
   } catch(const string &e) {
