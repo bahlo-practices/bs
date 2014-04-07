02db0c63d06229e2ffd5df363d2dfc4a2751a22c
Arne Bahlo <hallo@arne.me>
2014-04-07 11:15:52 +0200

    Add basic input logic

--- practice2/main.cpp
+++ practice2/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <exception>
 
 #include "colors.h"
@@ -10,14 +9,26 @@ int main() {
   setlocale(0, "DE");
 
   try {
+    string input = "";
     cout << BLUE << "Welcome to Trash! Exit with <logout>" << RESET << endl;
+    while (true) {
+      cout << "$ ";
+      cin >> input;
+
+      if (input == "logout") {
+        break;
+      } else if (input != "") {
+        cout << "COMMAND: " << input << endl;
+      }
+    }
+    return 0;
   } catch(const string &e) {
-    cerr << e << endl;
+    cerr << RED << e << endl;
     return 1;
   } catch(const std::exception &e) {
-    cerr << e.what() << endl;
+    cerr << RED << e.what() << endl;
   } catch(...) {
-    cerr << "Ein unbekannter Fehler ist aufgetreten." << endl;
+    cerr << RED << "Ein unbekannter Fehler ist aufgetreten." << endl;
     return 2;
   }
 
